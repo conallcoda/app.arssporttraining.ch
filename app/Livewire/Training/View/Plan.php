32091fec1b5cf447f6b15e2b5f38393e2f3d0764
Conall O'Reilly <conall@coda.works>
2026-01-27 01:13:19 +0100

    done

--- app/Livewire/Training/View/Plan.php
+++ app/Livewire/Training/View/Plan.php
@@ -4,6 +4,10 @@ namespace App\Livewire\Training\View;
 
 use App\Livewire\Concerns\InteractsWithParentView;
 use App\Models\TrainingPlan;
+use App\Models\Users\User;
+use Illuminate\Database\Eloquent\Collection;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\Url;
 use Livewire\Component;
 
 class Plan extends Component
@@ -12,9 +16,36 @@ class Plan extends Component
 
     public TrainingPlan $trainingPlan;
 
+    #[Url]
+    public ?int $user = null;
+
     public function mount(TrainingPlan $trainingPlan): void
     {
         $this->trainingPlan = $trainingPlan;
+    }
+
+    #[Computed]
+    public function users(): Collection
+    {
+        return $this->trainingPlan->allUsers()
+            ->orderBy('forename')
+            ->orderBy('surname')
+            ->get();
+    }
+
+    #[Computed]
+    public function selectedUser(): ?User
+    {
+        if ($this->user === null) {
+            return null;
+        }
+
+        return $this->users->firstWhere('id', $this->user);
+    }
+
+    public function selectUser(int $userId): void
+    {
+        $this->user = $userId;
     }
 
     public function render()
--- resources/views/livewire/training/view/plan.blade.php
+++ resources/views/livewire/training/view/plan.blade.php
@@ -1,6 +1,23 @@
-<div>
-    <flux:card>
-        <flux:heading size="lg">Plan</flux:heading>
-        <flux:text class="mt-2">View and edit the training plan for {{ $trainingPlan->name }}.</flux:text>
-    </flux:card>
+<div class="flex gap-6">
+    <x-section title="Athletes" class="w-64 shrink-0">
+        <div class="flex flex-col gap-1">
+            @foreach ($this->users as $userItem)
+                <flux:button
+                    wire:click="selectUser({{ $userItem->id }})"
+                    variant="{{ $user === $userItem->id ? 'primary' : 'ghost' }}"
+                    class="justify-start"
+                >
+                    {{ $userItem->name }}
+                </flux:button>
+            @endforeach
+        </div>
+    </x-section>
+
+    <x-section title="Plan" class="flex-1">
+        @if ($this->selectedUser)
+            <flux:heading size="lg">{{ $this->selectedUser->name }}</flux:heading>
+        @else
+            <flux:text class="text-zinc-500">Select an athlete to view their plan.</flux:text>
+        @endif
+    </x-section>
 </div>
