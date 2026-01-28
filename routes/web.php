c07cdfd9b33f4041d3b2ed5494c58b0e2576ee9d
Conall O'Reilly <conall@coda.works>
2026-01-28 14:55:02 +0100

    done

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Livewire\BlockCreator;
 use App\Livewire\Calculator;
-use App\Livewire\Database;
 use App\Livewire\Database\AthleteGroupIndex;
 use App\Livewire\Database\AthleteIndex;
 use App\Livewire\Database\ExerciseIndex;
@@ -10,12 +9,12 @@ use App\Livewire\Training\TrainingPlanIndex;
 use App\Livewire\Training\TrainingPlanView;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', Database::class)->name('database');
+Route::redirect('/', '/training-plans');
 
 Route::get('/athletes', AthleteIndex::class)->name('athlete-index');
 Route::get('/athletes/groups', AthleteGroupIndex::class)->name('athlete-group-index');
 Route::get('/exercises', ExerciseIndex::class)->name('exercise-index');
 Route::get('/training-plans', TrainingPlanIndex::class)->name('training-plan-index');
 Route::get('/training-plans/{trainingPlan}', TrainingPlanView::class)->name('training-plan-view');
-Route::get('/schedule', BlockCreator::class)->name('block-creator');
-Route::get('/calculator', Calculator::class)->name('calculator');
+Route::get('/schedule-test', BlockCreator::class)->name('block-creator');
+//Route::get('/calculator', Calculator::class)->name('calculator');
