402b12e375c27d9e8390f3ae382db05cdea533b8
Conall O'Reilly <conall@coda.works>
2026-05-20 16:54:38 +0200

    a

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,13 +1,5 @@
-import Checklist from '@editorjs/checklist';
-import Delimiter from '@editorjs/delimiter';
-import EditorJS from '@editorjs/editorjs';
-import Header from '@editorjs/header';
-import List from '@editorjs/list';
-import Quote from '@editorjs/quote';
-import Table from '@editorjs/table';
 import Treeselect from 'treeselectjs';
 
-import { registerDocumentWriter } from '../../../../coda-packages/form-kit/resources/js/alpine';
 import '../../../../coda-packages/cms/resources/js/alpine';
 
 import './athlete-mobile-shell';
@@ -17,15 +9,5 @@ import './alpine/calendar-slot-popover';
 import './alpine/metric-cell-popover';
 import './alpine/editable-cell';
 import './alpine/org_chart_parse';
-
-registerDocumentWriter({
-    EditorJS,
-    Checklist,
-    Delimiter,
-    Header,
-    List,
-    Quote,
-    Table,
-});
 
 globalThis.Treeselect = Treeselect;
