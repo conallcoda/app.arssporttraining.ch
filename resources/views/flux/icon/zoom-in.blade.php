eac3a7cf6899ee78b648f0df4bcac2955db1d5b9
Conall O'Reilly <conall@coda.works>
2026-07-02 11:23:45 +0200

    a

--- resources/views/flux/icon/zoom-in.blade.php
+++ resources/views/flux/icon/zoom-in.blade.php
@@ -39,8 +39,8 @@ $strokeWidth = match ($variant) {
     aria-hidden="true"
     data-slot="icon"
 >
-  <rect width="7" height="9" x="3" y="3" rx="1" />
-  <rect width="7" height="5" x="14" y="3" rx="1" />
-  <rect width="7" height="9" x="14" y="12" rx="1" />
-  <rect width="7" height="5" x="3" y="16" rx="1" />
+  <circle cx="11" cy="11" r="8" />
+  <path d="m21 21-4.35-4.35" />
+  <path d="M11 8v6" />
+  <path d="M8 11h6" />
 </svg>
